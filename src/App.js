205835804c3c71f93d78020083c156ee5d0e4fa1
Anastasia Danilova <anastasia.proekt@gmail.com>
2023-03-26 12:05:37 +0300

    changed filders names

--- src/App.js
+++ src/App.js
@@ -1,9 +1,9 @@
 import "./App.css";
 
-import Header from "./assets/components/Header/Header";
-import Table from "./assets/components/AllWordsTable/AllWordsTable";
-import Footer from "./assets/components/Footer/Footer";
-import Card from "./assets/components/Card/Card";
+import Header from "./assets/components/header/Header";
+import Table from "./assets/components/allWordsTable/AllWordsTable";
+import Footer from "./assets/components/footer/Footer";
+import Card from "./assets/components/card/Card";
 
 function App() {
   return (
