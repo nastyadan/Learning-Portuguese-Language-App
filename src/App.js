81d2ec9d8ef97b8f276cfdcf989623b02b42f905
Anastasia Danilova <anastasia.proekt@gmail.com>
2023-04-30 03:54:28 +0300

    added routing

--- src/App.js
+++ src/App.js
@@ -1,27 +1,95 @@
 import "./App.css";
-import Header from "./assets/components/header/Header.jsx";
+import React from "react";
+import { BrowserRouter, Routes, Route, Link } from "react-router-dom";
+//import Header from "./assets/components/header/Header.jsx";
+import headerStyles from "./assets/components/header/Header.module.scss";
 import AllWordsTable from "./assets/components/allWordsTable/AllWordsTable.jsx";
-import Footer from "./assets/components/footer/Footer.jsx";
+//import Footer from "./assets/components/footer/Footer.jsx";
+import footerStyles from "./assets/components/footer/Footer.module.scss";
 import Slider from "./assets/components/cardSlider/CardSlider";
 import { listOfWords } from "./assets/data/Data.js";
 import "bootstrap/dist/css/bootstrap.min.css";
 function App() {
   return (
-    <div className="App">
-      <Header />
-
-      <Slider
-        portuguese={listOfWords.portuguese}
-        transcription={listOfWords.transcription}
-        russian={listOfWords.russian}
-        key={listOfWords.id}
-      />
-
-      <AllWordsTable
-        couldbechanged={listOfWords.map((couldbechanged) => couldbechanged)}
-      />
-      <Footer />
-    </div>
+    <BrowserRouter>
+      <div className="App">
+        <header>
+          <div className={headerStyles.header}>
+            <nav className={headerStyles.nav}>
+              <Link to="/">
+                <button className={headerStyles.buttonInNav}>На главную</button>
+              </Link>
+              <Link to="/table">
+                <button className={headerStyles.buttonInNav}>
+                  Список слов
+                </button>
+              </Link>
+              <Link to="/cards">
+                <button className={headerStyles.buttonInNav}>
+                  Смотреть карточки
+                </button>
+              </Link>
+            </nav>
+          </div>
+        </header>
+        <main>
+          <Routes>
+            <Route
+              path="/cards"
+              element={
+                <Slider
+                  portuguese={listOfWords.portuguese}
+                  transcription={listOfWords.transcription}
+                  russian={listOfWords.russian}
+                  key={listOfWords.id}
+                />
+              }
+            />
+            <Route
+              path="/table"
+              element={
+                <AllWordsTable
+                  couldbechanged={listOfWords.map(
+                    (couldbechanged) => couldbechanged
+                  )}
+                />
+              }
+            />
+            <Route
+              exact
+              path="/"
+              element={
+                <AllWordsTable
+                  couldbechanged={listOfWords.map(
+                    (couldbechanged) => couldbechanged
+                  )}
+                />
+              }
+            />
+            <Route path="*" element="Ошибка 404: Страница не найдена" />
+          </Routes>
+        </main>
+        <footer>
+          <div className={footerStyles.footer}>
+            <nav className={footerStyles.nav}>
+              <Link to="/">
+                <button className={footerStyles.buttonInNav}>На главную</button>
+              </Link>
+              <Link to="/table">
+                <button className={footerStyles.buttonInNav}>
+                  Список слов
+                </button>
+              </Link>
+              <Link to="/cards">
+                <button className={footerStyles.buttonInNav}>
+                  Смотреть карточки
+                </button>
+              </Link>
+            </nav>
+          </div>
+        </footer>
+      </div>
+    </BrowserRouter>
   );
 }
 
--- src/assets/components/header/Header.jsx
+++ src/assets/components/header/Header.jsx
@@ -1,17 +1,23 @@
 import React from "react";
-//import headerStyles from "./Header.module.scss";
 import headerStyles from "../footer/Footer.module.scss";
+import { Link } from "react-router-dom";
 
 export default class Header extends React.Component {
   render() {
     return (
       <div className={headerStyles.footer}>
         <nav className={headerStyles.nav}>
-          <button className={headerStyles.buttonInNav}>Все слова</button>
-          <button className={headerStyles.buttonInNav}>Мой словарь</button>
-          <button className={headerStyles.buttonInNav}>
-            Начать тренировку
-          </button>
+          <Link to="/home">
+            <button className={headerStyles.buttonInNav}>На главную</button>
+          </Link>
+          <Link to="/table">
+            <button className={headerStyles.buttonInNav}>Список слов</button>
+          </Link>
+          <Link to="/cards">
+            <button className={headerStyles.buttonInNav}>
+              Смотреть карточки
+            </button>
+          </Link>
         </nav>
       </div>
     );
