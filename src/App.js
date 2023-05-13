244cde08611068c9a547704f8dd14d9fb25cfee9
Anastasia Danilova <anastasia.proekt@gmail.com>
2023-05-14 01:59:09 +0300

    added position fixed for header

--- src/App.js
+++ src/App.js
@@ -1,11 +1,11 @@
 import "./App.css";
 import React from "react";
-import { BrowserRouter, Routes, Route, Link } from "react-router-dom";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Header from "./assets/components/header/Header.jsx";
 //import headerStyles from "./assets/components/header/Header.module.scss";
 import AllWordsTable from "./assets/components/allWordsTable/AllWordsTable.jsx";
-//import Footer from "./assets/components/footer/Footer.jsx";
-import footerStyles from "./assets/components/footer/Footer.module.scss";
+import Footer from "./assets/components/footer/Footer.jsx";
+//import footerStyles from "./assets/components/footer/Footer.module.scss";
 import Slider from "./assets/components/cardSlider/CardSlider";
 import { listOfWords } from "./assets/data/Data.js";
 import "bootstrap/dist/css/bootstrap.min.css";
@@ -42,25 +42,7 @@ function App() {
             <Route path="*" element="Ошибка 404: Страница не найдена" />
           </Routes>
         </main>
-        <footer>
-          <div className={footerStyles.footer}>
-            <nav className={footerStyles.nav}>
-              <Link to="/">
-                <button className={footerStyles.buttonInNav}>На главную</button>
-              </Link>
-              <Link to="/table">
-                <button className={footerStyles.buttonInNav}>
-                  Список слов
-                </button>
-              </Link>
-              <Link to="/cards">
-                <button className={footerStyles.buttonInNav}>
-                  Смотреть карточки
-                </button>
-              </Link>
-            </nav>
-          </div>
-        </footer>
+        <Footer />
       </div>
     </BrowserRouter>
   );
