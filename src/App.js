677440fc066423daae84fbe1aa077005d6b6c7e3
Anastasia Danilova <anastasia.proekt@gmail.com>
2023-05-27 02:22:24 +0300

    added context

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import "./App.css";
 import React from "react";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { ApiContext } from "./assets/components/apiContext/ApiContext";
 import Header from "./assets/components/header/Header.jsx";
 import AllWordsTable from "./assets/components/allWordsTable/AllWordsTable.jsx";
 import Footer from "./assets/components/footer/Footer.jsx";
@@ -8,42 +9,81 @@ import Slider from "./assets/components/cardSlider/CardSlider";
 import { listOfWords } from "./assets/data/Data.js";
 import "bootstrap/dist/css/bootstrap.min.css";
 
-function App() {
-  return (
-    <BrowserRouter>
-      <div className="App">
-        <Header />
-        <main>
-          <Routes>
-            <Route path="/cards" element={<Slider {...listOfWords} />} />
-            <Route
-              path="/table"
-              element={
-                <AllWordsTable
-                  couldbechanged={listOfWords.map(
-                    (couldbechanged) => couldbechanged
-                  )}
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      words: [],
+      isLoading: false,
+      error: null,
+    };
+  }
+
+  componentDidMount() {
+    this.setState({ isLoading: true });
+    fetch("http://itgirlschool.justmakeit.ru/api/words")
+      .then((response) => {
+        console.log(response);
+        if (response.ok) {
+          return response.json();
+        } else {
+          throw new Error("Что-то пошло не так(");
+        }
+      })
+      .then((response) => {
+        this.setState({
+          words: response,
+          isLoading: false,
+        });
+      })
+      .catch((error) => this.setState({ error, isLoading: false }));
+  }
+  render() {
+    const { words, isLoading, error } = this.state;
+    if (error) {
+      return <p>{error.message}</p>;
+    }
+    if (isLoading) {
+      return <p>Идёт загрузка ...</p>;
+    }
+    return (
+      <ApiContext.Provider value={words}>
+        <BrowserRouter>
+          <div className="App">
+            <Header />
+            <main>
+              <Routes>
+                <Route path="/cards" element={<Slider {...listOfWords} />} />
+                <Route
+                  path="/table"
+                  element={
+                    <AllWordsTable
+                      couldbechanged={listOfWords.map(
+                        (couldbechanged) => couldbechanged
+                      )}
+                    />
+                  }
                 />
-              }
-            />
-            <Route
-              exact
-              path="/"
-              element={
-                <AllWordsTable
-                  couldbechanged={listOfWords.map(
-                    (couldbechanged) => couldbechanged
-                  )}
+                <Route
+                  exact
+                  path="/"
+                  element={
+                    <AllWordsTable
+                      couldbechanged={listOfWords.map(
+                        (couldbechanged) => couldbechanged
+                      )}
+                    />
+                  }
                 />
-              }
-            />
-            <Route path="*" element="Ошибка 404: Страница не найдена" />
-          </Routes>
-        </main>
-        <Footer />
-      </div>
-    </BrowserRouter>
-  );
+                <Route path="*" element="Ошибка 404: Страница не найдена" />
+              </Routes>
+            </main>
+            <Footer />
+          </div>
+        </BrowserRouter>
+      </ApiContext.Provider>
+    );
+  }
 }
 
 export default App;
